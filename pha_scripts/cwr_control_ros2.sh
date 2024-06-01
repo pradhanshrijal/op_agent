d27c27ce19e4003617f117d135c4de6aca20d0ad
Shrijal Pradhan <shrijal.pradhan.company.man@gmail.com>
2024-06-01 08:31:36 +0200

    PHA Scripts

---
+++ pha_scripts/cwr_control_ros2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+
+OP_ENVS=$1
+CWR_ENVS=$2
+OP_ENVS="${OP_EVNS:=${OP_BRIDGE_ROOT}/pha_scripts/export_exploration_envs.sh}"
+CWR_ENVS="${CWR_ENVS:=${OP_BRIDGE_ROOT}/pha_scripts/export_cwr_envs.sh}"
+
+source ${OP_ENVS}
+source ${CWR_ENVS}
+
+echo "Starting CWR Control .. "
+echo "-------------------------------"
+
+# ros2 launch /home/hatem/carla/op_agent/autoware_carla_launch/carla_autoware_sensors_interface.xml
+ros2 launch ${OP_AGENT_ROOT}/autoware_carla_launch/modules/cwr_control.launch.xml
+# $SHELL
+
